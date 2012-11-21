364b8aa7c087c67c1cbe699e7f7928b3c1970b82
kaxap <kashimov@gmail.com>
2012-11-21 19:25:25 +0600

    refactoring hook

--- hook/skype_interc.dpr
+++ hook/skype_interc.dpr
@@ -4,11 +4,11 @@ uses
   SysUtils, Windows, Messages;
 
   type
-    Tjmp_far = packed record
+    TJmpFar = packed record
       instr_push: BYTE;
       arg: DWORD;
       instr_ret: BYTE;
-	end;
+    end;
 
 const
   LIB_NAME = 'gdi32.dll';
@@ -16,9 +16,13 @@ const
   ID_URL = 0;
   ID_FALLBACK = 1;
 
+  STR_MAIN_FORM_CLASS = 'TfrmSkypePopup';
+  STR_MAIN_FORM_CAPTION = 'SkypePopup5738';
+  LINK_COLOR = $FF9933;
+
 var
   g_hhook1, g_hhook2, g_hhook3: Integer;
-  origExtTextOutW: TJmp_far;
+  origExtTextOutW: TJmpFar;
   prev_link: String;
 
 function ShowError(const s: String): String;
@@ -27,47 +31,62 @@ begin
 end;
 
 procedure InterceptFunction(pszCalleeModName: PChar; pszFuncName: PChar;
-  pfnHook: pointer; var read: TJMP_far; setup: boolean);
-var adr: Integer;
-    w, w1: cardinal;
-    write:TJMP_FAR;
+  pfnHook: pointer; var read: TJmpFar; setup: boolean);
+//intecepts function with name pszFuncName in module pszCalleeModName
+var
+   adr: Integer;
+   w, w1: cardinal;
+   write: TJmpFar;
 begin
-    //MessageBox(0, 'Intercepting.', '', 0);
-    adr := Integer(GetProcAddress(GetModuleHandle(pszCalleeModName), pszFuncName));
-    if adr = 0 then
+  //MessageBox(0, 'Intercepting.', '', 0);
+  adr := Integer(GetProcAddress(GetModuleHandle(pszCalleeModName),
+    pszFuncName));
+
+  if adr = 0 then
+  begin
+    ShowError('GetProcAddress returned 0');
+    Exit;
+  end;
+
+  {if NOT VirtualProtect(pointer(adr), sizeof(write), PAGE_EXECUTE_READWRITE, w) then
+  begin
+    ShowError('VirtualProtect failed.');
+  end;}
+
+  if setup then
+  begin
+
+    if NOT ReadProcessMemory(GetCurrentProcess(), pointer(adr), @read,
+      sizeof(read), w1) then
     begin
-      ShowError('GetProcAddress returned 0');
+      ShowError('ReadProcessMemory failed');
     end;
 
-    {if NOT VirtualProtect(pointer(adr), sizeof(write), PAGE_EXECUTE_READWRITE, w) then
-    begin
-      ShowError('VirtualProtect failed.');
-    end;}
+    write.instr_push := $68;		// push pfnHook
+    write.arg := dword(pfnHook);
+    write.instr_ret := $C3;		// ret
 
-    if setup then
+    if NOT WriteProcessMemory(GetCurrentProcess(), pointer(adr), @write,
+       sizeof(write), w1) then
     begin
-      if NOT ReadProcessMemory(GetCurrentProcess(),pointer(adr), @read, sizeof(read), w1) then
-      begin
-        ShowError('ReadProcessMemory failed');
-      end;
+      ShowError('WriteProcessMemory failed');
+    end;
 
-      write.instr_push := $68;		// push pfnHook
-      write.arg := dword(pfnHook);
-      write.instr_ret := $C3;		// ret
-      if NOT WriteProcessMemory(GetCurrentProcess(), pointer(adr), @write, sizeof(write), w1) then
-      begin
-        ShowError('WriteProcessMemory failed');
-      end;
-    end else
-      if NOT WriteProcessMemory(GetCurrentProcess(), pointer(adr), @read, sizeof(read), w1) then
-      begin
-        ShowError('WriteProcessMemory (2) failed');
-      end;
+  end else
+  begin
 
-    {if NOT VirtualProtect(pointer(adr), sizeof(write), w, nil) then
+    if NOT WriteProcessMemory(GetCurrentProcess(), pointer(adr), @read,
+      sizeof(read), w1) then
     begin
-      ShowError('VirtualProtect failed');
-    end;}
+      ShowError('WriteProcessMemory (2) failed');
+    end;
+    
+  end;
+
+  {if NOT VirtualProtect(pointer(adr), sizeof(write), w, nil) then
+  begin
+    ShowError('VirtualProtect failed');
+  end;}
 end;
 
 procedure sendUrl(const s: String);
@@ -75,9 +94,10 @@ var
   wnd: HWND;
   data: CopyDataStruct;
 begin
-  wnd := FindWindow('TfrmSkypePopup', 'SkypePopup5738');
+  wnd := FindWindow(STR_MAIN_FORM_CLASS, STR_MAIN_FORM_CAPTION);
   if wnd > 0 then
   begin
+    //send data via WM_COPYDATA message
     data.dwData := ID_URL;
     data.cbData := Length(s) + 1;
     data.lpData := PChar(s);
@@ -91,9 +111,10 @@ var
   wnd: HWND;
   data: CopyDataStruct;
 begin
-  wnd := FindWindow('TfrmSkypePopup', 'SkypePopup5738');
+  wnd := FindWindow(STR_MAIN_FORM_CLASS, STR_MAIN_FORM_CAPTION);
   if wnd > 0 then
   begin
+    //send data via WM_COPYDATA message
     data.dwData := ID_FALLBACK;
     data.cbData := Length(s) + 1;
     data.lpData := PChar(s);
@@ -101,7 +122,8 @@ begin
   end;
 end;
 
-function checkMetrics(dc: HDC): Boolean;
+function checkUnderlined(dc: HDC): Boolean;
+// check whether text in given HDC is underlined
 var
   tm: TEXTMETRICA;
 begin
@@ -116,34 +138,42 @@ begin
   result := ExtTextOutW(DC, X, Y, Options, Rect, Str, Count, Dx);
   InterceptFunction(LIB_NAME, FUNC_NAME , @ExtTextOUtW__, origExtTextOutW, true);
 
-  if (Copy(Str, 1, 5) = 'http:') AND (GetTextColor(DC) = $FF9933) then
+  //if text begins with http and has link color
+  if (Copy(Str, 1, 5) = 'http:') AND (GetTextColor(DC) = LINK_COLOR) then
   begin
-    if checkMetrics(DC) then
-      sendUrl(Str) else
+    if checkUnderlined(DC) then
     begin
+      //send url to main app
+      sendUrl(Str);
+    end else
+    begin
+      //send <fallback> when mouse not hovering over the previous link anymore
       if Str = prev_link then
         sendFallback(Str);
     end;
   end;
-    //MessageBox(0, PChar(Format('%x', [GetTextColor(DC)])), '', 0);
+
 end;
 
 procedure LibraryProc(Reason: Integer);
 var s:string;
 begin
+  //get process name
   s := ExtractFileName(ParamStr(0));
-  if AnsiLowerCase(s) <> 'skype.exe' then Exit; // тут правь имя проги, за которой наблюдаешь
 
-  //MessageBox(0, PChar(s), nil, 0);
-  //Windows.Beep(1000, 100);
+  //inject in skype.exe only
+  if AnsiLowerCase(s) <> 'skype.exe' then Exit;
+
   case Reason of
   DLL_PROCESS_ATTACH:
   begin
+    //inject
     InterceptFunction(LIB_NAME, FUNC_NAME , @ExtTextOutW__, origExtTextOutW, true);
   end;
 
   DLL_PROCESS_DETACH:
   begin
+    //restore
     InterceptFunction(LIB_NAME, FUNC_NAME , nil, origExtTextOutW, false);
   end;
 end;
